02d1fb785bdf16c366bbf6f789c1b37c313244b7
siddharth-singh-git <siddharth.singh0195@gmail.com>
2026-06-04 18:25:37 +0530

    3rd Project complete csv reading

---
+++ 3-csv/src/main.rs
@@ -0,0 +1,18 @@
+use csv;
+use std::error::Error;
+
+fn read_from_csv(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+
+    for result in reader.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = read_from_csv("./customers.csv") {
+        eprintln!("{}", e);
+    }
+}
