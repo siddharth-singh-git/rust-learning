fc2cb12f82576789c2e78cbfcc73ea7b2d387468
siddharth-singh-git <siddharth.singh0195@gmail.com>
2026-06-05 09:28:28 +0530

    6 got requsr complete

--- 6-get-request/src/main.rs
+++ 6-get-request/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+    println!("{}", body);
+    Ok(())
 }
