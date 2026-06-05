f3758cd72350e593888f991c84bb00fb30f30e7a
siddharth-singh-git <siddharth.singh0195@gmail.com>
2026-06-05 13:20:26 +0530

    async complete

--- 7-async-await/src/main.rs
+++ 7-async-await/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let res = reqwest::get("https://httpbin.org/get").await?;
+    println!("Status: {}", res.status());
+    println!("Headers:\n{:#?}", res.headers());
+
+    let body = res.text().await?;
+    println!("Body \n{}", body);
+    Ok(())
 }
