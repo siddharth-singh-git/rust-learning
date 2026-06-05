9631fb3087cce286856741350bd933cc092ee5d5
siddharth-singh-git <siddharth.singh0195@gmail.com>
2026-06-05 17:20:23 +0530

    9 auth complete

---
+++ 9-auth/src/main.rs
@@ -0,0 +1,17 @@
+use reqwest::blocking::Client;
+use reqwest::Error;
+
+fn main() -> Result<(), Error> {
+    let client = Client::new();
+
+    let user = "user1".to_string();
+    let passwd: Option<String> = None;
+
+    let response = client
+        .get("http://httpbin.org/")
+        .basic_auth(user, passwd)
+        .send();
+
+    println!("{:?}", response);
+    Ok(())
+}
